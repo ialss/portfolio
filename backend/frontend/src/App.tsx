050699d093a2cedae531287ddf3bad2edba3b53f
ialss <klaidaswik@gmail.com>
2025-05-09 16:39:14 -0400

    initial dev commit

--- backend/frontend/src/App.tsx
+++ backend/frontend/src/App.tsx
@@ -19,7 +19,7 @@ function Main() {
 
     const fetchProjects = async () => {
         try {
-            const response = await fetch("https://www.klaidasw.com/api/projects/")
+            const response = await fetch("http://127.0.0.1:8000/api/projects/")
             const data = await response.json();
             setProjects(data);
         } catch (err) {
